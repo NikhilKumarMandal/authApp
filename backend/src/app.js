bc01422be937a8c79da87de8c164e93385778c70
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-05-30 20:31:20 +0530

    basic setup for socket.io

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,37 +1,50 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import http from "http";
+import { Server } from "socket.io";
 
-const app = express()
+const app = express();
+const httpServer = http.createServer(app);
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
+// Initialize Socket.io
+const io = new Server(httpServer, {
+    pingTimeout: 60000,
+    cors: {
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    },
+});
+
+// Handle Socket.io connection
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+app.set("io", io);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+import chatRouter from "./routes/chat.routes.js";
+import messageRouter from "./routes/message.routes.js";
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/chat", chatRouter);
+app.use("/api/v1/message", messageRouter);
 
 
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
-
-
-
-import userRouter from "./routes/user.routes.js"
-import chatRouter from "./routes/chat.routes.js"
-import messageRouter from "./routes/message.routes.js"
-
-
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/chat", chatRouter)
-app.use("/api/v1/message", messageRouter)
-
-
-
-
-
-
-export { app }
+export { app, httpServer };
